05d43943f6715180143b88180826ebcaef71ac4a
Alpha01001 <anaslamik@gamil.com>
2023-09-11 12:18:30 -0700

    Hello, puts

--- 0x00-hello_world/4-puts.c
+++ 0x00-hello_world/4-puts.c
@@ -1,6 +1,6 @@
 #include <stdio.h>
 /**
- * mian - A program that prints line with puts function
+ * main - A program that prints line with puts function
  * Return: 0 (Success)
  */
 int main(void)
